d3801b293824855519608ace9dcc4907ba8ebf7c
慈父斯大林 <abokecn@hotmail.com>
2026-04-16 21:23:05 +0800

    Remove site configuration and use default config

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -1,27 +1,3 @@
 import { defineValaxyConfig } from 'valaxy'
 
-export default defineSiteConfig({
-  lang: 'zh-CN',
-  title: 'Valaxy Theme Yun',
-  url: 'https://valaxy.site/',
-  author: {
-    name: '云游君',
-    avatar: 'https://www.yunyoujun.cn/images/avatar.jpg',
-  },
-  /**
-   * 站点图标
-   */
-  favicon: 'https://www.yunyoujun.cn/favicon.svg',
-  /**
-   * 副标题
-   */
-  subtitle: 'All at sea.',
-  description: 'Valaxy Theme Yun Preview.',
-  social: [
-    {
-      name: 'RSS',
-      link: '/atom.xml',
-      icon: 'i-ri-rss-line',
-      color: 'orange',
-  },
-})
+export default defineValaxyConfig({})
